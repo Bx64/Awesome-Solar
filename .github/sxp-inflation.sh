53487da1e45f2c7712c6d654493bf1b8edfabf38
BFGX <65274887+Bx64@users.noreply.github.com>
2022-04-03 18:28:35 +0200

    Release: 1.0.8
    Awesome Solar update:
    - Add some colour and more detailed information to 'sxp-inflation.sh' script, that calculates yearly inflation on the Solar blockchain.

--- .github/sxp-inflation.sh
+++ .github/sxp-inflation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
 # genesisUnixTs = 1648490400 || 1800 UTC, 28th of March 2022
 # secondsInYear = 31557600   || seconds in one year (365.25 * 24 * 60 * 60)
 # genesisSupply = 520737576  || https://explore.solar/blocks/e492f9d34df01238fda8bac79be19cb77656f444347d076a55811f909eb24e3a
@@ -7,4 +11,12 @@
 currentUnixTs=$(date +%s)
 currentSupply=$(curl -sS supply.mainnet.sh)
 
+echo ""
+echo -e "${RED}Genesis supply was:${ENDCOLOR}"
+echo "520737576.00000000 SXP"
+echo -e "${RED}Current supply is:${ENDCOLOR}"
+echo "$currentSupply SXP"
+echo ""
+echo -e "${GREEN}One year inflation percentage:${ENDCOLOR}"
 bc -l <<< "scale=10; (((((($currentSupply - 520737576)) / 520737576)) * 100 * ((31557600 / (($currentUnixTs - 1648490400))))))"
+echo ""
